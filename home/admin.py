beb1f04df8aa15ad9256c60d84c02dcffafc298f
manasbobade2023 <72960975+manasbobade2023@users.noreply.github.com>
2022-11-29 09:22:53 +0530

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from home.models import Contact
 from . import models
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from .models import User
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(models.Question)
